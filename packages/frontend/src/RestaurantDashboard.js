d5a246d46da34c463884308491ce81add846eee6
Pierre Criulanscy <pcriulan@gmail.com>
2020-10-12 18:15:13 +0200

    feat(home): add description texts

--- packages/frontend/src/RestaurantDashboard.js
+++ packages/frontend/src/RestaurantDashboard.js
@@ -1,7 +1,20 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
-import { Spinner, Box, Image, Flex, Text, Button } from "@chakra-ui/core";
+import {
+  Spinner,
+  Box,
+  Image,
+  Flex,
+  Text,
+  List,
+  ListItem,
+  ListIcon,
+  Button,
+  Link,
+} from "@chakra-ui/core";
+import { RiContactsBookFill } from "react-icons/ri";
+import { AiOutlineQrcode, AiOutlineExport } from "react-icons/ai";
 import { AuthStateContext } from "./AuthContext";
 import { SignUpForm } from "./SignUpForm";
 import { ContactList } from "./ContactList";
@@ -36,8 +49,35 @@ export const RestaurantDashboard = ({
   }
   return (
     <Box>
-      <Flex justifyContent="center">
-        <Image src={logo} alt="Resto Covid"></Image>
+      <Flex justifyContent="center" direction="column">
+        <Image
+          src={logo}
+          alt="Resto Covid"
+          size={300}
+          alignSelf="center"
+        ></Image>
+        <List spacing={4} marginBottom="1.5em">
+          <ListItem>
+            <ListIcon icon={RiContactsBookFill} color="blue.500"></ListIcon>
+            Resto Covid vous permet de mettre en place gratuitement votre cahier
+            de rappel numérique qui répond aux{" "}
+            <Link href="https://www.cnil.fr/fr/covid-19-et-les-cahiers-de-rappel-les-recommandations-de-la-cnil">
+              exigences de la CNIL
+            </Link>{" "}
+            quant à la protection des données de vos clients.
+          </ListItem>
+          <ListItem>
+            <ListIcon icon={AiOutlineQrcode} color="blue.500"></ListIcon>
+            Enregistrez votre restaurant et bénéficiez d'un QR code à mettre à
+            disposition de vos clients pour qu'ils puissent communiquer leur
+            informations de contact.
+          </ListItem>
+          <ListItem>
+            <ListIcon icon={AiOutlineExport} color="blue.500"></ListIcon>
+            En cas de détection de cluster, vous pouvez en un clic exporter les
+            données des 14 derniers jours.
+          </ListItem>
+        </List>
       </Flex>
       <Box bg="teal.100" p={4} marginBottom={"1em"} marginTop={"1em"}>
         <Text textAlign="center">
